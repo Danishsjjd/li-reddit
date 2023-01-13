11d198bbebe9601afa4c51627c5f559e581e98a4
DanishSjjd <danishsjjad@gmail.com>
2023-01-14 00:38:53 +0500

    fix: migrate apollo-server-express to graphql-yoga

--- server/src/startup/middleware.ts
+++ server/src/startup/middleware.ts
@@ -1,19 +1,19 @@
-import { ApolloServer } from "apollo-server-express"
 import type { Express } from "express"
 import { MyContext } from "src/type"
 import { buildSchema } from "type-graphql"
+import { createYoga } from "graphql-yoga"
 
 import { HelloResolver } from "../resolver/hello"
 import PostResolver from "../resolver/post"
 
 export default async function (app: Express, em: MyContext["em"]) {
-  const apolloServer = new ApolloServer({
+  const yoga = createYoga({
     schema: await buildSchema({
       resolvers: [HelloResolver, PostResolver],
       // validate: false,
     }),
     context: () => ({ em }),
   })
-  await apolloServer.start()
-  apolloServer.applyMiddleware({ app })
+
+  app.use("/graphql", yoga)
 }
