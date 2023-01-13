e50f3930448e1f248b92ae4247ffb97e6eae5820
DanishSjjd <danishsjjad@gmail.com>
2023-01-13 19:58:51 +0500

    feat: user can delete post

--- server/src/resolver/post.ts
+++ server/src/resolver/post.ts
@@ -1,23 +1,52 @@
 import { Post } from "../entities/Post"
 import { Arg, Ctx, Mutation, Query, Resolver } from "type-graphql"
 import { MyContext } from "src/type"
+import { Loaded } from "@mikro-orm/core"
 
 @Resolver()
 export default class PostResolver {
   @Query(() => [Post])
-  posts(@Ctx() { em }: MyContext) {
+  posts(@Ctx() { em }: MyContext): Promise<Loaded<Post>[]> {
     return em.find(Post, {})
   }
 
   @Query(() => Post, { nullable: true })
-  post(@Arg("id") id: number, @Ctx() { em }: MyContext) {
+  post(
+    @Arg("id") id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<Loaded<Post | null>> {
     return em.findOne(Post, { id })
   }
 
   @Mutation(() => Post)
-  async createPost(@Arg("title") title: string, @Ctx() { em }: MyContext) {
+  async createPost(
+    @Arg("title") title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Loaded<Post>> {
     const post = em.create(Post, { title })
     await em.persistAndFlush(post)
     return post
+  }
+
+  @Mutation(() => Post, { nullable: true })
+  async updatePost(
+    @Arg("id") id: number,
+    @Arg("title") title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Loaded<Post | never> | null> {
+    const post = await em.findOne(Post, { id })
+    if (!post) return null
+    post.title = title
+    await em.persistAndFlush(post)
+    return post
+  }
+
+  @Mutation(() => Boolean)
+  async deletePost(
+    @Arg("id") id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<boolean> {
+    await em.nativeDelete(Post, { id })
+    return true
   }
 }
