bbd2521f3fd2012276b42ce512322c0a167207de
DanishSjjd <danishsjjad@gmail.com>
2023-01-16 20:35:28 +0500

    feat: add web (next.js)

---
+++ web/src/pages/register.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Register = () => {
+  return (
+    <section className="">
+      <button className="btn btn-primary">click me</button>
+    </section>
+  )
+}
+
+export default Register
